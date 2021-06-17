f1b6595657fde288c2e0019f6d6d8e8233c7b0a9
T4rtP1ck13 <85512841+T4rtP1ck13@users.noreply.github.com>
2021-06-17 10:00:53 -0500

    Added even more to "Installer.sh"

--- Installer.sh
+++ Installer.sh
@@ -1,13 +1,24 @@
- #!/bin/bash
- echo "Compiling..."
+#!/usr/bin/env bash
+echo "Compiling..."
 clang++ Calculator.cpp CalculatorFunctions.cpp -std=c++17 -o CalculatorExec
 echo "Done!"
 echo "Cleaning up..."
 sleep 1
 mkdir ${HOME}/Calculator
 mv CalculatorExec ${HOME}/Calculator
-rm -rf  ${HOME}/Terminal-Based-Calculator
-echo "Done!" 
+mv Calculator.desktop ${HOME}/Desktop
+desktop_file='Calculator.desktop'
+cat >"$desktop_file" <<-EOF
+	[Desktop Entry]
+	Name=Calculator
+	Exec=${HOME}/Calculator/CalculatorExec
+    Terminal=true
+    Comment=Calculator for simple math problems.
+    Type=application
+	...
+EOF
+rm -rf ${HOME}/Terminal-Based-Calculator
+echo "Done!"
 echo "Now enjoy your calculator :)!"
 echo "Closing in:"
 echo "3"
